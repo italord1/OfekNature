8063482f47c742085a8feae413d6701904bbc25a
Itai Galipoliti <itai.galipo@gmail.com>
2025-12-03 19:15:27 +0200

    skelaton need to fix some

--- OfekNature-Frontend/OfekNature/src/Pages/About.jsx
+++ OfekNature-Frontend/OfekNature/src/Pages/About.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import "../Styles/About.css";
 import { BaseUrlContext } from "../Context/BaseUrl";
 
--- OfekNature-Frontend/OfekNature/src/Pages/Home.jsx
+++ OfekNature-Frontend/OfekNature/src/Pages/Home.jsx
@@ -1,11 +1,15 @@
-import "../Styles/Home.css"
+import "../Styles/Home.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="home">
       <section className="hero">
-        <h1>Nature With ofek</h1>
-        <p>הדרכות טבע, טיולים וסדנאות חווייתיות</p>
+        <div className="hero-content">
+          <h1>Nature With Ofek</h1>
+          <p>הדרכות טבע, טיולים וסדנאות חווייתיות</p>
+          <Link to="/gallery" className="btn">Explore Gallery</Link>
+        </div>
       </section>
     </div>
   );
