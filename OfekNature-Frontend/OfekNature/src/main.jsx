3028b1f886b3d917cf759c053a521eddadcc8342
Itai Galipoliti <itai.galipo@gmail.com>
2025-12-04 13:20:52 +0200

    Update main.jsx

--- OfekNature-Frontend/OfekNature/src/main.jsx
+++ OfekNature-Frontend/OfekNature/src/main.jsx
@@ -5,10 +5,10 @@ import App from './App.jsx'
 import { BaseUrlProvider } from "./Context/BaseUrl.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ 
      <BaseUrlProvider>
       <App />
      </BaseUrlProvider>
     
-  </StrictMode>,
+  
 )
