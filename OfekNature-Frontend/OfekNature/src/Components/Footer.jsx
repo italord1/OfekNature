09169ff3cfae93ec1636690030ceff23612f4218
Itai Galipoliti <itai.galipo@gmail.com>
2025-12-06 13:52:17 +0200

    Update Footer.jsx

--- OfekNature-Frontend/OfekNature/src/Components/Footer.jsx
+++ OfekNature-Frontend/OfekNature/src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import "../Styles/Footer.css"
 export default function Footer() {
   return (
     <footer className="footer">
-      <p> © 2025 OfeKNature — כל הזכויות שמורות. built by Itai Galipoliti</p>
+    <p dir="rtl">© 2025 OfeKNature — כל הזכויות שמורות. built by Itai Galipoliti</p>
     </footer>
   );
 }
